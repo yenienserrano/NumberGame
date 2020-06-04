fe63ae0d6b07488cd90588fb51e5c3797e0d7f60
Ian <yenienserrano@live.com.ar>
2020-06-03 22:47:31 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'numbergame';
+export class AppComponent implements OnInit {
+  title = 'Podras adivinar el numero??';
+  number: number;
+  estado: string;
+  numeroSeleccionado: number;
+
+  constructor(){
+  }
+
+  ngOnInit(): void{
+    this.numeroRandom()   
+  }
+
+  verificarNumero(){
+    if(this.numeroSeleccionado > this.number){
+      this.estado = "bajo"
+      this.number = null
+
+    }else if(this.numeroSeleccionado < this.number){
+      this.estado = "alto"
+      this.number = null
+    }else{
+      this.estado = "correcto"
+      this.number = null
+    }
+  }
+
+  numeroRandom(){
+    var aleatorio = Math.round(Math.random()*100);
+    this.numeroSeleccionado = aleatorio
+    this.estado = ''
+    this.number = null
+  }
+
 }
